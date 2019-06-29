2111dd7241c249036e7b8234e3856636f4cc4a68
PHT-Leon <52360940+PHT-Leon@users.noreply.github.com>
2019-06-30 00:51:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print("HELLO WE ARE PHT TEAM")
+print("HELLO WE ARE PHT TEAM"\n"that test")
+
+fname = "input your first name"
+
+
+lname = "input your last name "
+print(input(,fname,lname)       
+
 
